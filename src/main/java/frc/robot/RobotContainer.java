6e947e369c995451a0e8d2e9e6c2354293ded792
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2025-01-05 10:40:51 -0600

    Pivot default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,39 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.PivotPercentageCommand;
+import frc.robot.constants.IOConstants;
+import frc.robot.constants.PivotConstants;
+import frc.robot.subsystems.Pivot;
 
 public class RobotContainer {
+
+  private final Joystick driverJoystick;
+  private final Joystick placerJoystick;
+
+  private final Pivot pivot;
+
   public RobotContainer() {
+
+    pivot = Pivot.getInstance();
+
+    driverJoystick = new Joystick(IOConstants.driverJoystickPort);
+    placerJoystick = new Joystick(IOConstants.placerJoystickPort);
+
     configureBindings();
+
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    pivot.setDefaultCommand(
+        new PivotPercentageCommand(
+            pivot,
+            () -> placerJoystick.getRawAxis(0) * PivotConstants.maxPercentageOutput));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- src/main/java/frc/robot/constants.java
+++ src/main/java/frc/robot/constants.java
@@ -6,6 +6,15 @@ public class constants {
     public static final class PivotConstants{
 
         public static final double positionConversionFactor = Math.PI * 2;
-        
+
+        public static final double maxPercentageOutput = 0.3;
+    }
+
+    public static final class IOConstants {
+
+        public static final int driverJoystickPort = 0;
+        public static final int placerJoystickPort = 1;
+
+
     }
 }
