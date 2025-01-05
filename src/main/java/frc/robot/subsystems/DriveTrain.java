1b9edc4abab6198bb3a65b488649aa52b31c6c69
GrePaC <A01366730@tec.mx>
2025-01-05 10:16:39 -0600

    Commands folder created

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,4 +1,6 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class DriveTrain {
     
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,6 +1,26 @@
 package frc.robot.subsystems;
-import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-public class Intake {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class Intake extends SubsystemBase {
+    private SparkMax intakeMotor;
+    private int intakeID = 1;
+
+    public Intake(){
+
+        intakeMotor = new SparkMax(intakeID, MotorType.kBrushless);
+    }
+
+    public void runIntake(double speed){
+        intakeMotor.set(speed);
+    }
+
+    public void stopIntake(){
+        intakeMotor.set(0);
+    }
+
 }
