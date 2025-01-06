e30f2785209ecdea9bb8f67547e4fe01e5d8987f
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2025-01-06 10:17:49 -0600

    Prueba intake

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,73 +6,121 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 /**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * The methods in this class are called automatically corresponding to each
+ * mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the
+ * package after creating
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
 
-  VictorSPX rightBack = new VictorSPX(10);
-  VictorSPX rightFront = new VictorSPX(20);
-  VictorSPX leftBack = new VictorSPX(30);
-  VictorSPX leftFront = new VictorSPX(40);
+    VictorSPX rightBack = new VictorSPX(10);
+    VictorSPX rightFront = new VictorSPX(20);
+    VictorSPX leftBack = new VictorSPX(30);
+    VictorSPX leftFront = new VictorSPX(40);
 
-  Joystick control = new Joystick(0);
+    // SparkMax wristMotor = new SparkMax(7, MotorType.kBrushless);
+    SparkMax intakeMotor = new SparkMax(7, MotorType.kBrushless);
 
-  
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  public Robot() {
-    rightBack.setInverted(leftBack.getInverted());
-    rightFront.setInverted(leftBack.getInverted());
-    leftBack.setInverted(true);
-    leftFront.setInverted(true);
-  }
+    AbsoluteEncoder absoluteEncoder;
 
-  @Override
-  public void robotPeriodic() {}
+    RelativeEncoder relativeEncoder;
 
-  @Override
-  public void autonomousInit() {}
+    Joystick control = new Joystick(0);
 
-  @Override
-  public void autonomousPeriodic() {}
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
+     */
+    public Robot() {
+        rightBack.setInverted(leftBack.getInverted());
+        rightFront.setInverted(leftBack.getInverted());
+        leftBack.setInverted(true);
+        leftFront.setInverted(true);
 
-  @Override
-  public void teleopInit() {
+        // absoluteEncoder = wristMotor.getAbsoluteEncoder();
+        // relativeEncoder = wristMotor.getEncoder();
+    }
 
-  }
+    @Override
+    public void robotPeriodic() {
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    rightBack.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(5)*0.5);
-    rightFront.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(5)*0.5);
-    leftBack.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(1)*0.5);
-    leftFront.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(1)*0.5);
-  }
+    @Override
+    public void autonomousInit() {
+    }
 
-  @Override
-  public void disabledInit() {}
+    @Override
+    public void autonomousPeriodic() {
+    }
 
-  @Override
-  public void disabledPeriodic() {}
+    @Override
+    public void teleopInit() {
 
-  @Override
-  public void testInit() {}
+    }
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void teleopPeriodic() {
+        // rightBack.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(5)*0.5);
+        // rightFront.set(VictorSPXControlMode.PercentOutput,
+        // control.getRawAxis(5)*0.5);
+        // leftBack.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(1)*0.5);
+        // leftFront.set(VictorSPXControlMode.PercentOutput, control.getRawAxis(1)*0.5);
 
-  @Override
-  public void simulationInit() {}
+        // if (control.getRawAxis(1) > 0.1) {
+        //     wristMotor.set(control.getRawAxis(1) * 0.5);
+        // } else {
+        //     wristMotor.set(control.getRawAxis(5 ) * -0.5);
+        // }
 
-  @Override
-  public void simulationPeriodic() {}
+        if(control.getRawButton(1)){
+            intakeMotor.set(0.2);
+        } else if(control.getRawButton(2)){
+            intakeMotor.set(0.3);
+        } else if(control.getRawButton(3)){
+            intakeMotor.set(0.4);
+        } else if(control.getRawButton(4)){
+            intakeMotor.set(0.5);
+        } else if(control.getRawButton(5)){
+            intakeMotor.set(0.6);
+        } else {
+            intakeMotor.set(0);
+        }
+
+    }
+
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    @Override
+    public void testInit() {
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void simulationInit() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    }
 }
